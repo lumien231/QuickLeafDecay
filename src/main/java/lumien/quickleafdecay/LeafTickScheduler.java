f7acd7224c0572a3f01b6579b9b20573dd6e646a
Lumien <marian.kreiser@googlemail.com>
2019-08-06 11:18:54 +0200

    Update to MC 1.14.4

--- src/main/java/lumien/quickleafdecay/LeafTickScheduler.java
+++ src/main/java/lumien/quickleafdecay/LeafTickScheduler.java
@@ -9,8 +9,8 @@ import net.minecraft.block.BlockState;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.fml.LogicalSide;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class LeafTickScheduler
 {
